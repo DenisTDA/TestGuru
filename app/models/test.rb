cf8cbd0383f2c8eb62c07f76d55579fcf354f8ee
Denis Toritsyn <iadtden@gmail.com>
2022-02-26 01:09:49 +0500

    fix: syntacs & bugs with .gitignore

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
-  def self.list_tests(category_title)
+   def self.list_tests(category_title)
     joins('JOIN categories ON tests.category_id=categories.id')
-          .where('categories.title = :title', title: category_title)
+          .where(categories: {title: category_title})
           .order(title: :desc)
           .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
     Test.joins('JOIN results ON tests.id = results.test_id')
-            .where('tests.level = :level AND results.user_id = :id' , level: level, id: self.id)
+          .where({level: level} && {results: {user_id: self.id}})
   end
 end
