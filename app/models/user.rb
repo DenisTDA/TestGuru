85c709c3e7263e545220571aadfbcc157fb91786
Denis Toritsyn <iadtden@gmail.com>
2022-02-25 13:36:27 +0500

    fix: task3 fix bags

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   def tests_by_level(level)
     Result.joins('JOIN tests ON tests.id = results.test_id')
             .where('tests.level = :level AND results.user_id = :id' , level: level, id: self.id)
-            .pluck('tests.title')
   end
 end
