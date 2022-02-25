cb8c3bdc6209292022ee4b96eb1de2c655284a78
Denis Toritsyn <iadtden@gmail.com>
2022-02-25 21:08:19 +0500

    feat: task3 revision 2, fix all bugs

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.list_tests(category_title)
+    joins('JOIN categories ON tests.category_id=categories.id')
+          .where('categories.title = :title', title: category_title)
+          .order(title: :desc)
+          .pluck(:title)
+  end
 end
