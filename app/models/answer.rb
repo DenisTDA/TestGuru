c98f6097b670577b4a13b608e8daca92c526be6a
Denis Toritsyn <iadtden@gmail.com>
2022-03-06 19:19:10 +0500

    fix: fix Test, add composity key in Test & add migration

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,14 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_range, on: [ :create, :update ]
+  validate :validate_range, on: :create
 
   scope :correct, -> { where(correct: true) }
- 
+
   private
+
   def validate_range
-    if question.answers.count >= 4 
-      errors.add(:base, "Have to contain 1 - 4 answers") 
-    end
+    errors.add(:base, 'Have to contain 1 - 4 answers') if question.answers.count >= 4
   end
 end
