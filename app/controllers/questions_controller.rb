ba264e0cd726a96e8f66b188618d81e772706e14
Denis Toritsyn <iadtden@gmail.com>
2022-03-10 13:32:31 +0500

    fix: fix action_destry, fix veiw_new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,36 +1,46 @@
 class QuestionsController < ApplicationController
-  before_action :define_test, only: %i[index create]
-  skip_before_action :verify_authenticity_token
+  layout false
+  before_action :set_test, only: %i[index create new]
+  before_action :set_question, only: %i[show destroy]
+  #skip_before_action :verify_authenticity_token
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    list_questions = define_test.questions.inspect
-    render plain: list_questions.split('>,').join("\n")
+    list_questions = @test.questions.inspect
+    render plain: list_questions.split('#').join("\n")
   end
 
   def show
-    question = Question.find(params[:id]).inspect
-    render plain: question
+    render inline: "<p> <%= @question.inspect %> </p>
+    <%= link_to 'delete', @question, :method => 'delete' %>"
+#    <%= button_to 'delete', @question, :method => 'delete' %>
   end
 
   def new; end
 
   def create
-    question = define_test.questions.create!(question_param)
-    render plain: question.inspect
+    @question = @test.questions.build(question_param)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   def destroy
-    @question = Question.find(params[:id]).inspect
-    Question.delete(params[:id])
-    render inline: '<p> The question <%= @question %> was deleted</p>'
+    @question.delete
+    render inline: '<p> The question <%= @question.inspect %> was deleted</p>'
   end
 
   protected
 
-  def define_test
-    Test.find(params[:test_id])
+  def set_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 
   def question_param
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  
   resources :tests do
     resources :questions, shallow: true
   end
-
-  get "/questions/:id/:delete", to: 'questions#destroy'
 end
