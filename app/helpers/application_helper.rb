819ba55cb3629e3effedcb621de1f65f204e8fd9
Denis Toritsyn <iadtden@gmail.com>
2022-03-28 13:14:14 +0500

    fix: fix some bugs in task9

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def show_flash(type)
     return unless flash[type]
 
-    form_flash = "<div class = 'flash'><div class = 'flash #{type}'>#{flash[type]}<\/div><\/div>"
+    form_flash = content_tag(:p, content_tag(:p, flash[type], class: "flash #{type}"), class: "flash")
     form_flash.html_safe
   end
 end
