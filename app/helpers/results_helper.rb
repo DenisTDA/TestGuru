28f1bf46aa9654eca5d8779d9a53728981694b36
Denis Toritsyn <iadtden@gmail.com>
2022-04-08 14:50:35 +0500

    feat: task12, add admin panel

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -2,12 +2,10 @@ module ResultsHelper
   def output_result(result)
     html_str = if result.successful?
                  "You passed the test!<br>
-                  Your result is
-                  <div class = 'done'>"
+                 Your result is <div class = 'text-success'>"
                else
                  " You failed the test!<br>
-                  Your result is
-                  <div class = 'failure'>"
+                  Your result is <div class = 'text-danger'>"
                end
     html_str.concat(result.percentage_result.to_s, "<\/div>", ' %').html_safe
   end
