dbb6e30e138ed782bb01d2b22cb5443616fb0e51
Denis Toritsyn <iadtden@gmail.com>
2022-04-18 18:45:49 +0500

    fix: task13, correct gist controller

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -5,15 +5,13 @@ class GistsController < ApplicationController
 
     gist_result = GistQuestionService.new(@question).call
 
-    gist = Gist.new(html_url: gist_result.url, question_id: @question.id, user_id: current_user.id)
-
-    flash_options = if gist_result.success?
-                      gist.save!
-                      { notice: t('.success', url_gist: view_context.link_to(gist_result[:url], gist_result[:url],
+    if gist_result.success?
+      Gist.create!(html_url: gist_result.url, question_id: @question.id, user_id: current_user.id)      
+      flash_options = { notice: t('.success', url_gist: view_context.link_to(gist_result.url, gist_result.url,
                                                                              rel: 'nofollow noopener', target: '_blank')) }
-                    else
-                      { alert: t('.failure') }
-                    end
+    else
+      flash_options = { alert: t('.failure') }
+    end
     redirect_to @result, flash_options
   end
 end
