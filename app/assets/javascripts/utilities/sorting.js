a42d88b92c754dd1505ea7b9d9475e6fd836ca6e
Denis Toritsyn <iadtden@gmail.com>
2022-05-24 21:42:20 +0500

    fix: task16, sorting.js

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -1,6 +1,7 @@
 document.addEventListener('turbolinks:load', function(){
   const iconsClass = { up : '.octicon-chevron-up', down : '.octicon-chevron-down' }
   const table = document.querySelector('table')
-  const cellTh = table?.querySelector('.sort-by-title')
-  if (table && cellTh) { new SortTable(table, cellTh, iconsClass) }
+  const cellTh = null
+  if (table) { cellTh = table.querySelector('.sort-by-title') }
+  if (cellTh) { new SortTable(table, cellTh, iconsClass) }
 })
